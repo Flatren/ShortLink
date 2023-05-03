dbe5fe61e296728e36afc078b7f35317755d87e9
Flatren <45997535+Flatren@users.noreply.github.com>
2023-05-03 21:56:53 +0500

    readme

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,8 @@ import json
 from flask import request, Flask
 from app import App
 import os
-
+from app.controllers.get_link_controller import controller_api as get_link_controller
+from app.controllers.get_short_link_controller import controller_api as get_short_link_controller
 app_flask = Flask(__name__)
 """print(os.environ["DBNAME"])
 print(os.environ["HOST"])
@@ -17,24 +18,6 @@ app = App(dbname=os.environ["DBNAME"],
           password=os.environ["PASSWORD"],
           port=os.environ["PORT"])
 
-
-@app_flask.route("/api/get_short_link", methods=['POST'])
-def api_get_short_link():
-    link = request.get_json().get('link')
-    app_flask.logger.log(level=logging.INFO, msg=str(request.args))
-    if link is None or link == "":
-        return {"ERROR": "Пустая ссылка."}
-    return {"response": app.get_short_link(link)}
-
-
-@app_flask.route("/api/get_link", methods=['POST'])
-def api_get_link():
-    link = request.get_json().get('hash')
-    app_flask.logger.log(level=logging.INFO, msg=str(request.args))
-    if link is None:
-        return {"ERROR": "Пустая ссылка."}
-    result = app.get_long_link(link)
-    return {"response": result}
-
-
+app_flask.route("/api/get_short_link", methods=['POST'])(get_short_link_controller(app))
+app_flask.route("/api/get_link", methods=['POST'])(get_link_controller(app))
 app_flask.run(host='0.0.0.0', port=int(os.environ["PORT_EXEC"]))
